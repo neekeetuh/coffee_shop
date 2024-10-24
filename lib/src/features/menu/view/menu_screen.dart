81c0a73075f64aab013ca8ed03b1c75232a9f02d
neekeetuh <nikitakhimchenko@gmail.com>
2024-10-25 00:24:07 +0600

    created initial menu screen along with the item card widget, added assets folder with a couple of templates

--- lib/src/features/menu/view/menu_screen.dart
+++ lib/src/features/menu/view/menu_screen.dart
@@ -1,5 +1,8 @@
+import 'package:coffee_shop/src/app.dart';
 import 'package:coffee_shop/src/features/menu/models/menu_category.dart';
+import 'package:coffee_shop/src/features/menu/models/menu_item.dart';
 import 'package:coffee_shop/src/theme/app_colors.dart';
+import 'package:coffee_shop/src/theme/image_sources.dart';
 import 'package:flutter/material.dart';
 
 class MenuScreen extends StatefulWidget {
@@ -31,9 +34,10 @@ class _MenuScreenState extends State<MenuScreen> {
                       padding: const EdgeInsets.all(8),
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(16)),
-                      selectedColor: isActive ? AppColors.primaryColor : Colors.white,
+                      selectedColor:
+                          isActive ? AppColors.primaryColor : AppColors.white,
                       labelStyle: TextStyle(
-                          color: isActive ? Colors.white : Colors.black,
+                          color: isActive ? AppColors.white : AppColors.black,
                           fontSize: 14),
                       label: Text(widget.categories[index].title),
                       selected: isActive,
@@ -52,6 +56,40 @@ class _MenuScreenState extends State<MenuScreen> {
                 ),
               ),
             ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              sliver: SliverList.builder(
+                  itemCount: widget.categories.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            widget.categories[index].title,
+                            style: const TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w600),
+                          ),
+                          GridView(
+                            shrinkWrap: true,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 16,
+                              mainAxisSpacing: 16,
+                            ),
+                            children:  [
+                              MenuItemCard(item:itemsMock[0]),
+                              MenuItemCard(item:itemsMock[1]),
+                              MenuItemCard(item:itemsMock[2]),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
           ],
         ),
       ),
@@ -62,5 +100,60 @@ class _MenuScreenState extends State<MenuScreen> {
     setState(() {
       chosenIndex = index;
     });
+  }
+}
+
+class MenuItemCard extends StatelessWidget {
+  const MenuItemCard({
+    super.key,
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 180,
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              item.image,
+              height: 100,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                item.title,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 24,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: AppColors.primaryColor),
+              child: Text(
+                '${item.price} руб',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: AppColors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
